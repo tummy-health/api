44a462fc308f296e3d2a5f7a672f8aaf70d9a3b0
taylor <taylor.laekeman@gmail.com>
2022-06-22 18:26:37 -0400

    add linters

--- src/handler.ts
+++ src/handler.ts
@@ -1,13 +1,14 @@
 const handler = async (event) => ({
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully (yay typescript)!",
-        input: event,
-      },
-      null,
-      2
-    ),
+  statusCode: 200,
+  body: JSON.stringify(
+    {
+      message:
+        'Go Serverless v3.0! Your function executed successfully (yay typescript)!',
+      input: event,
+    },
+    null,
+    2
+  ),
 });
 
 export default handler;
